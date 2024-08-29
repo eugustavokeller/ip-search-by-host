855ca22027707fb8c0eef8c973c7b8498ef2171d
Gustavo Keller <gustavo.lealtec@gmail.com>
2024-08-29 14:13:59 -0300

    add func to NS search

--- app/app.go
+++ app/app.go
@@ -16,24 +16,32 @@ func Gerar() *cli.App {
 	app.Usage = "Busca IPs e Nomes de Servidores na internet"
 	app.Version = "0.0.1"
 
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "google.com.br",
+		},
+	}
+
 	app.Commands = []cli.Command{
 		{
-			Name:  "ip",
-			Usage: "Busca IPs de endereços na internet",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "host",
-					Value: "google.com.br",
-				},
-			},
-			Action: buscarIps,
+			Name:   "ip",
+			Usage:  "Busca IPs de endereços na internet",
+			Flags:  flags,
+			Action: searchIps,
+		},
+		{
+			Name:   "servers",
+			Usage:  "Busca o nome dos servidores na internet",
+			Flags:  flags,
+			Action: searchServers,
 		},
 	}
 
 	return app
 }
 
-func buscarIps(c *cli.Context) {
+func searchIps(c *cli.Context) {
 	host := c.String("host")
 
 	ips, err := net.LookupIP(host)
@@ -43,5 +51,18 @@ func buscarIps(c *cli.Context) {
 
 	for _, ip := range ips {
 		fmt.Println(ip)
+	}
+}
+
+func searchServers(c *cli.Context) {
+	host := c.String("host")
+
+	servers, err := net.LookupNS(host)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, server := range servers {
+		fmt.Println(server.Host)
 	}
 }
